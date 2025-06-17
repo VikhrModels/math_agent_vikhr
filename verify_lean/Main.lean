2f054f197c3932f26a86bf7cec64d3b7846e74ae
umbra2728 <umbra2728@gmail.com>
2025-06-18 01:32:36 +0300

    added theorem and import files

--- verify_lean/Main.lean
+++
@@ -1,4 +0,0 @@
-import VerifyLean
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
--- verify_lean/VerifyLean.lean
+++ verify_lean/VerifyLean.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `VerifyLean` library.
 -- Import modules here that should be built as part of the library.
-import VerifyLean.Basic
+import VerifyLean.theorem
+import VerifyLean.minif2f_import
