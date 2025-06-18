0ff9a042372da464def872f3ae1299f9b1e06ec1
umbra2728 <umbra2728@gmail.com>
2025-06-18 17:20:44 +0300

    Returned to original state verify_lean

--- verify_lean/VerifyLean.lean
+++ verify_lean/VerifyLean.lean
@@ -1,4 +1,3 @@
 -- This module serves as the root of the `VerifyLean` library.
 -- Import modules here that should be built as part of the library.
-import VerifyLean.theorem
-import VerifyLean.minif2f_import
+import VerifyLean.Basic
